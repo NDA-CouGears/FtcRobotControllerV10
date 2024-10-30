1e393c3d6614aa7573e89b350094b875fecabdad
27jrobinson <27jrobinson@ndahingham.com>
2024-10-30 17:46:17 -0400

    Update MecanumDriveNDA.java
    Lift works with hold buttons and left joystick.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveNDA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveNDA.java
@@ -34,35 +34,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
+
 
 @TeleOp(name="Main Drive", group="Drive")
 public class MecanumDriveNDA extends LinearOpMode {
@@ -73,33 +48,40 @@ public class MecanumDriveNDA extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
-    private Servo claw_servo;
+
+    private Servo claw = null;
+
+    private DcMotor armMotor = null;
+
+    private TouchSensor touchSensor = null;
+
+    final private static double ClawClosed = 0.68;
+    final private static double ClawOpen = 0.5;
 
     @Override
     public void runOpMode() {
 
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "lf_drive");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "lb_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rf_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rb_drive");
-        //claw_servo = hardwareMap.get(Servo.class, "claw");
 
-        // ########################################################################################
-        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-        // ########################################################################################
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        armMotor = hardwareMap.get(DcMotor.class, "lift");
+
+        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        claw.setDirection(Servo.Direction.FORWARD);
 
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
@@ -108,8 +90,8 @@ public class MecanumDriveNDA extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-        int claw_pos = 5000;
-
+        boolean holdingAtA = false;
+        boolean holdingAtB = false;
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             double max;
@@ -139,33 +121,6 @@ public class MecanumDriveNDA extends LinearOpMode {
                 rightBackPower  /= max;
             }
 
-            if (gamepad1.dpad_left) {
-                claw_pos -= 1;
-            }
-            if (claw_pos < 0)
-                claw_pos = 0;
-            if (gamepad1.dpad_right) {
-                claw_pos += 1;
-            }
-            if (claw_pos > 10000)
-                claw_pos = 10000;
-            //claw_servo.setPosition(claw_pos/10000.0);
-
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
 
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
@@ -178,12 +133,63 @@ public class MecanumDriveNDA extends LinearOpMode {
             int lbp = leftBackDrive.getCurrentPosition();
             int rbp = rightBackDrive.getCurrentPosition();
 
+            if (gamepad2.y){
+                holdingAtA = false;
+                holdingAtB = false;
+            }
+            else if ((gamepad2.a) || (holdingAtA)) {
+                armMotor.setTargetPosition(2000);
+                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotor.setPower(1);
+
+                while (opModeIsActive() && armMotor.isBusy() && (!gamepad2.y)) {
+                    idle();
+                }
+
+                armMotor.setPower(0);
+                if (!gamepad2.y) {
+                    holdingAtA = true;
+                }
+            }
+            else if ((gamepad2.b) || (holdingAtB)) {
+                armMotor.setTargetPosition(4700);
+                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armMotor.setPower(1);
+
+                while (opModeIsActive() && armMotor.isBusy() && (!gamepad2.y)) {
+                    idle();
+                }
+
+                armMotor.setPower(0);
+                if (!gamepad2.y){
+                    holdingAtB = true;
+                }
+            }
+            else {
+                double armMotorPower = -gamepad2.right_stick_y;
+                armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                if ((touchSensor.isPressed()) && (armMotorPower < 0)) {
+                    armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                }
+
+                else {
+                    armMotor.setPower(armMotorPower);
+                }
+
+                telemetry.addData("Motor power: ", armMotor.getPower());
+                telemetry.addData("Motor position: ", armMotor.getCurrentPosition());
+            }
+
+
+
+
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Encoders lf, rf, lb, rb", "%d, %d, %d, %d", lfp, rfp, lbp, rbp);
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Claw position:", "%d", claw_pos);
             telemetry.update();
         }
-    }}
+    }
+}
