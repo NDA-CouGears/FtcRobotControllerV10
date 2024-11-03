9a77693a5c6118b79226ed8fa9622d7c250218b9
Cougears-NDA <cougears@ndaHingham.com>
2024-11-03 11:19:35 -0500

    first autonomous attempt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,6 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.acmerobotics.roadrunner.Vector2d;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous
 
@@ -9,9 +19,46 @@ public class Autonomous extends RobotParent {
     @Override
     public void runOpMode() throws InterruptedException {
         initHardware();
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+
+        // Delcare Trajectory as such
+        Action TrajectoryAction1 = drive.actionBuilder(drive.pose)
+                .lineToX(50)
+                .build();
+
+        Action TrajectoryAction2 = drive.actionBuilder(new Pose2d(15,20,0))
+                .splineTo(new Vector2d(5,5), Math.toRadians(45))
+                .build();
 
         waitForStart();
 
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        TrajectoryAction1, // Example of a drive action
+
+                        // This action and the following action do the same thing
+                        new Action() {
+                            @Override
+                            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                                telemetry.addLine("Action!");
+                                telemetry.update();
+                                return false;
+                            }
+                        },
+                        // Only that this action uses a Lambda expression to reduce complexity
+                        (telemetryPacket) -> {
+                            telemetry.addLine("Action!");
+                            telemetry.update();
+                            return false; // Returning true causes the action to run again, returning false causes it to cease
+                        },
+                        drive.actionBuilder(new Pose2d(15,10,Math.toRadians(125))) // Another way of running a trajectory (not recommended because trajectories take time to build and will slow down your code, always try to build them beforehand)
+                                .splineTo(new Vector2d(25, 15), 0)
+                                .build()
+
+                )
+        );
 
 
     }
