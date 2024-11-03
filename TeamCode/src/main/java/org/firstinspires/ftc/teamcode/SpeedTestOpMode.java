f9e51c2d54329ba326fdf77e2eccef6acc70a685
Bill Clogston <bill@clogstons.com>
2024-11-03 10:18:11 -0500

    Test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpeedTestOpMode.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,19 +13,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-@TeleOp(name="Communications Test", group="Tests")
+@TeleOp(name="Hardware Test", group="Tests")
 public class SpeedTestOpMode  extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
-    Servo servo;
+    DcMotor lift;
+    Servo claw;
     IMU imu;
 
     @Override
     public void runOpMode() throws InterruptedException {
         /////////////////////////////////////////////
         // OpMode initialization
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.UP;
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
         // Now initialize the IMU with this mounting orientation
@@ -32,7 +35,13 @@ public class SpeedTestOpMode  extends LinearOpMode {
         imu.initialize(new IMU.Parameters(orientationOnRobot));
         imu.resetYaw();
 
-        servo = hardwareMap.get(Servo.class, "left_hand");
+        double claw_pos = 0;
+        claw = hardwareMap.get(Servo.class, "claw");
+        //arm = hardwareMap.get(Servo.class, "arm");
+
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        lift.setZeroPowerBehavior(DcMotor. ZeroPowerBehavior.BRAKE);
+        lift.setDirection(DcMotorSimple.Direction.REVERSE);
 
         /////////////////////////////////////////////
         // Initialization complete, wait for start
@@ -44,21 +53,39 @@ public class SpeedTestOpMode  extends LinearOpMode {
         // Run op mode
         runtime.reset();
 
-        if (servo != null) {
-            for (int i = 0; i < 1000; i++) {
-                servo.setPosition(i/1000.0);
-                servo.getPosition();
-            }
-        }
-
         String runTime = runtime.toString();
 
         while (opModeIsActive()) {
             telemetry.addData("Status", "Test Run Time: " + runTime);
 
+            if (claw != null) {
+                if (gamepad1.dpad_left) {
+                    claw_pos -= 0.01;
+                }
+                if (gamepad1.dpad_right) {
+                    claw_pos += 0.01;
+                }
+                if (claw_pos > 1) {
+                    claw_pos = 1;
+                }
+                else if (claw_pos < 0) {
+                    claw_pos = 0;
+                }
+                claw.setPosition(claw_pos);
+            }
+
+            if (lift != null) {
+                double lift_delta = gamepad1.left_stick_y;
+
+                lift.setPower(lift_delta);
+            }
+
             // Retrieve Rotational Angles and Velocities
             YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
             AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
+
+            telemetry.addData("Lift position", lift.getCurrentPosition());
+            telemetry.addData("Claw position", claw.getPosition());
 
             telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
             telemetry.addData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
