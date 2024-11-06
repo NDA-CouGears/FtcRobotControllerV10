69f3403fccf690c0649da2ff1ebf1d93fb121003
27jrobinson <27jrobinson@ndahingham.com>
2024-11-06 18:00:59 -0500

    Methods in Robot Parent
    Put logic from TeleOp in methods in Robot Parent

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParent.java
@@ -24,6 +24,15 @@ public abstract class RobotParent extends LinearOpMode {
     final protected static double ClawClosed = 0.32;
     final protected static double ClawOpen = 0.14;
 
+    private double signPreserveSquare(double value) {
+
+        if (value > 0) {
+            return value * value;
+        }
+        else {
+            return -(value * value);
+        }
+    }
 
     public void initHardware(){
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "lf_drive");
@@ -52,5 +61,107 @@ public abstract class RobotParent extends LinearOpMode {
         // Wait for the game to start (driver presses START)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+    }
+
+    protected void mDrive(){
+        double max;
+        // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+        double axial = signPreserveSquare(gamepad1.left_stick_y*-0.9); // Remember, this is reversed!
+        double lateral = signPreserveSquare(gamepad1.left_stick_x * 0.7); // Counteract imperfect strafing
+        double yaw = (signPreserveSquare(gamepad1.right_stick_x * 1))*0.5;
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower  = axial + lateral + yaw;
+        double rightFrontPower = axial - lateral - yaw;
+        double leftBackPower   = axial - lateral + yaw;
+        double rightBackPower  = axial + lateral - yaw;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower  /= max;
+            rightFrontPower /= max;
+            leftBackPower   /= max;
+            rightBackPower  /= max;
+        }
+
+        // Send calculated power to wheels
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
+
+        int lfp = leftFrontDrive.getCurrentPosition();
+        int rfp = rightFrontDrive.getCurrentPosition();
+        int lbp = leftBackDrive.getCurrentPosition();
+        int rbp = rightBackDrive.getCurrentPosition();
+    }
+
+    protected void arm(){
+        boolean holdingAtA = false;
+        boolean holdingAtB = false;
+        if (gamepad2.y){
+            holdingAtA = false;
+            holdingAtB = false;
+        }
+        else if ((gamepad2.a) || (holdingAtA)) {
+            armMotor.setTargetPosition(2000);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armMotor.setPower(1);
+
+            while (opModeIsActive() && armMotor.isBusy() && (!gamepad2.y)) {
+                idle();
+            }
+
+            armMotor.setPower(0);
+            if (!gamepad2.y) {
+                holdingAtA = true;
+            }
+        }
+        else if ((gamepad2.b) || (holdingAtB)) {
+            armMotor.setTargetPosition(4700);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armMotor.setPower(1);
+
+            while (opModeIsActive() && armMotor.isBusy() && (!gamepad2.y)) {
+                idle();
+            }
+
+            armMotor.setPower(0);
+            if (!gamepad2.y){
+                holdingAtB = true;
+            }
+        }
+        else {
+            double armMotorPower = -gamepad2.right_stick_y;
+            armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            if ((touchSensor.isPressed()) && (armMotorPower < 0)) {
+                armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            }
+
+            else {
+                armMotor.setPower(armMotorPower);
+            }
+
+            telemetry.addData("Motor power: ", armMotor.getPower());
+            telemetry.addData("Motor position: ", armMotor.getCurrentPosition());
+        }
+    }
+
+    protected void claw(){
+        //pressing right bumper opens claw, left bumper closes claw
+        if (gamepad2.right_bumper){
+            claw.setPosition(ClawOpen);
+        }
+
+        else if (gamepad2.left_bumper){
+            claw.setPosition(ClawClosed);
+        }
     }
 }
