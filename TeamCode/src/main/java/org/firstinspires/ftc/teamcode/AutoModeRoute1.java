276a67dfbb131a8ee99c0eb9a55a7bf0f597343a
Cougears-NDA <cougears@ndaHingham.com>
2024-11-18 15:33:34 -0500

    o

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModeRoute1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModeRoute1.java
@@ -5,6 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 /*@Tess. This will (hopefully) be the auto mode that starts next to the basket,
 turns a bit, lifts up the sample and drops it in the top basket, file name is placeholder*/
 @Autonomous( name = "AutoMode1", group = "OpModes")
-public class AutoMode1 extends AutoMode {
+public class AutoModeRoute1 extends AutoMode {
 
 }
